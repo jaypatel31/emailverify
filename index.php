d7861d315f5e17ff0b253c2edecde23fe340b8b8
Jay Patel <jaypatel32157@gmail.com>
2020-08-12 14:16:04 +0530

    Updated UI

--- index.php
+++ index.php
@@ -1,53 +1,117 @@
 <?php
 session_start();
 ?>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-	<title>LOGIN</title>
-	<style>
-		h2{
-			color:red;
-		}
-		span{
-			color:orange;
-		}
-		.tr{
-			color:green;
-		}
-	</style>
+	<title>Login V2</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
 </head>
-<!--SIGNUP Section PHP-->
-
 <body>
 	
-	<form action="verification.php" method="post">
-		<fieldset>
-		<legend>SIGN UP</legend>
-		<label for="un">Email : </label><br/>
-		<input type="text" name="Un" id="un" required>
-		<br/><br/>
-		<label for="pwd">ENTER PASSWORD : </label><br/>
-		<input type="password" name="pwd" id="pwd" required placeholder="(a-z)(@#$%)(0-9)">
-		<br/><br/>
-		<label for="pwd2">RE-ENTER PASSWORD : </label><br/>
-		<input type="password" name="pwd2" id="pwd2" required >
-		<span><br/><br/>
-		<input type="submit" name="submit" value="SUBMIT"><br/><br/>
-		<h2 class="tr"></h2>
-		</fieldset>
-	</form>
-	<?php
-		if(isset($_SESSION)){
-			if(isset($_SESSION['match'])){
-				echo "<p style='color:red'>".$_SESSION['match']."</p>";
-				$_SESSION['match']="";
-				}
-			else if(isset($_SESSION['inv'])){
-				echo "<p style='color:red'>".$_SESSION['inv']."</p>";
-				$_SESSION['match']="";
-			}
-		}
-	?>
-</body>
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<form action="verification.php" method="post" class="login100-form validate-form">
+					<span class="login100-form-title p-b-26">
+						Welcome
+					</span>
+					<span class="login100-form-title p-b-20">
+						<i class="zmdi zmdi-font"></i>
+					</span>
+					<?php
+						if(isset($_SESSION)){
+							if(isset($_SESSION['match'])){
+								echo "<p style='color:red'>".$_SESSION['match']."</p>";
+								unset($_SESSION['match']);
+								}
+							else if(isset($_SESSION['inv'])){
+								echo "<p style='color:red;margin-bottom:15px;'>".$_SESSION['inv']."</p>";
+								unset($_SESSION['inv']);
+							}
+						}
+					?>
+					<div class="wrap-input100 validate-input" data-validate = "Valid email is: a@b.c">
+						<input class="input100" type="text" name="Un">
+						<span class="focus-input100" data-placeholder="Email"></span>
+					</div>
 
+					<div class="wrap-input100 validate-input" data-validate="Enter password">
+						<span class="btn-show-pass">
+							<i class="zmdi zmdi-eye"></i>
+						</span>
+						<input class="input100" type="password" name="pwd">
+						<span class="focus-input100" data-placeholder="Password"></span>
+					</div>
+					<div class="wrap-input100 validate-input" data-validate="Enter password">
+						<span class="btn-show-pass">
+							<i class="zmdi zmdi-eye"></i>
+						</span>
+						<input class="input100" type="password" name="pwd2">
+						<span class="focus-input100" data-placeholder="Re-enter Password"></span>
+					</div>
+					<div class="container-login100-form-btn">
+						<div class="wrap-login100-form-btn">
+							<div class="login100-form-bgbtn"></div>
+							<button type="submit" name="submit" value="SUBMIT" class="login100-form-btn">
+								Login
+							</button>
+						</div>
+					</div>
+
+					<div class="text-center p-t-20">
+						<span class="txt1">
+							Please Type Correct Email Address
+						</span>
+
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	
+
+	<div id="dropDownSelect1"></div>
+	
+<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
+</body>
 </html>
--- verification.php
+++ verification.php
@@ -19,22 +19,20 @@
 			$check4 = strlen($Password);
 			if($check2==1 && $check4 >= 6){
 				if($pmatch==$Password){
-				
-				$_SESSION['code']= rand(10000,99999);
-				$mailcont = $_SESSION['code'];
-				mail($UserName,"VERIFICATION MAIL",$mailcont);
-				//$false=false;
+					$_SESSION['code']= rand(10000,99999);
+					$mailcont = $_SESSION['code'];
+					mail($UserName,"VERIFICATION MAIL",$mailcont);
+					//$false=false;
 				}
 				else{
 					$_SESSION['match'] = "Password Doesn't Match";
-					
 					header('Location: index.php');
 				}
 			}
 			else{
-				$_SESSION['inv'] = "Please Write Password In The Given Format Only!!";
+				$_SESSION['inv'] = "Please Write Password In This((a-z)(@#$%)(0-9)) Given Format Only!!";
 				($check4>=6)?:$_SESSION['inv']="Password must container 6 or more charcter!!";
-				echo $_SESSION['inv'];
+				//echo $_SESSION['inv'];
 				//exit;
 				header('Location: index.php');
 			}
@@ -67,21 +65,92 @@
 ?>
 <html>
 <head>
-	<title>VERIFICATION</title>
+	<title>Verification Form</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
 </head>
-<body>
-<?php 
+<body>	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+			
+				<form action="verification.php" method="post" class="login100-form validate-form">
+					<span class="login100-form-title p-b-26">
+						Welcome To the Verification 
+					</span>
+					<?php 
 	if(isset($_SESSION['error'])){
-		echo "<p style='color:red'>".$_SESSION['error']."</p>";
+		echo "<p style='color:red;margin-bottom:20px;'>".$_SESSION['error']."</p>";
 		$_SESSION['error'] ="";
 	}
 ?>
-	<form method="post">
-		Please Enter The Verification Code Sent to Your Mail Address : 
-		<br/><br/><input type="text" name="code">
-		<br/><br/>
-		<input type="submit" name="verification" value="Verify">
-		<a href="index.php">Go BACK</a>
-	</form>
+					<div class="wrap-input100 validate-input" data-validate = "Valid email is: a@b.c">
+						<input class="input100" type="text" name="code">
+						<span class="focus-input100" data-placeholder="Verification Code"></span>
+					</div>
+					<div class="container-login100-form-btn">
+						<div class="wrap-login100-form-btn">
+							<div class="login100-form-bgbtn"></div>
+							<button type="submit" name="verification" value="Verify" class="login100-form-btn">
+								Verify
+							</button>
+						</div>
+					</div>
+
+					<div class="text-center p-t-20">
+						<span class="txt1">
+							Change The Email address
+						</span>
+						<a class="txt2" href="index.php">
+							Go Back
+						</a>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	
+
+	<div id="dropDownSelect1"></div>
+	
+<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
 </body>
 </html>
